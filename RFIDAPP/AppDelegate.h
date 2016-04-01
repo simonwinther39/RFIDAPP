47f66b4779ea079540877e99cbc4d5e2c9df0dbf
Apple Developer <Apple@bogon>
2016-04-01 06:08:12 -0700

    Initial Commit

---
+++ RFIDAPP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RFIDAPP
+//
+//  Created by Apple Developer on 4/1/16.
+//  Copyright © 2016 Apple Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
